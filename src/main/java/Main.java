0604f38cf71ecd505940b89dc0f719df339d76f3
Gitch14 <vladbuckach@ukr.net>
2022-02-24 19:06:13 +0200

    +Count nums

---
+++ src/main/java/Main.java
@@ -0,0 +1,245 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.sql.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Main extends Boolean{
+
+/*
+    public class DBConnect {
+        private Connection con;
+        private Statement st;
+        private ResultSet rs;
+
+        public DBConnect() {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://ps1.thehost.com.ua:3306/TestTaskMaven", "TestTaskMaven1", "Root12345");
+
+            } catch (Exception ex) {
+                System.out.println("Error: " + ex);
+            }
+        }
+    }
+
+    */
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String expressionText = scanner.nextLine();
+        List<Lexeme> lexemes = lexAnalyze(expressionText);
+        LexemeBuffer lexemeBuffer = new LexemeBuffer(lexemes);
+        System.out.println(expr(lexemeBuffer));
+
+
+        Pattern p = Pattern.compile("(\\d+)");
+        Matcher m = p.matcher(expressionText);
+        int count = 0;
+
+        while (m.find()) {
+            count++;
+        }
+
+        System.out.println("Количество чисел: " + count);
+
+    }
+
+    public enum LexemeType {
+        LEFT_BRACKET, RIGHT_BRACKET,
+        OP_PLUS, OP_MINUS, OP_MUL, OP_DIV,
+        NUMBER,
+        EOF;
+    }
+
+    public static class Lexeme {
+        LexemeType type;
+        String value;
+
+        public Lexeme(LexemeType type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        public Lexeme(LexemeType type, Character value) {
+            this.type = type;
+            this.value = value.toString();
+        }
+
+        @Override
+        public String toString() {
+            return "Lexeme{" +
+                    "type=" + type +
+                    ", value='" + value + '\'' +
+                    '}';
+        }
+    }
+
+    public static class LexemeBuffer {
+        private int pos;
+
+        public List<Lexeme> lexemes;
+
+        public LexemeBuffer(List<Lexeme> lexemes) {
+            this.lexemes = lexemes;
+        }
+
+        public Lexeme next() {
+            return lexemes.get(pos++);
+        }
+
+        public void back() {
+            pos--;
+        }
+
+        public int getPos() {
+            return pos;
+        }
+    }
+
+    public static List<Lexeme> lexAnalyze(String expText) {
+        ArrayList<Lexeme> lexemes = new ArrayList<>();
+        int pos = 0;
+        while (pos< expText.length()) {
+            char c = expText.charAt(pos);
+            switch (c) {
+                case '(':
+                    lexemes.add(new Lexeme(LexemeType.LEFT_BRACKET, c));
+                    pos++;
+                    continue;
+                case ')':
+                    lexemes.add(new Lexeme(LexemeType.RIGHT_BRACKET, c));
+                    pos++;
+                    continue;
+                case '+':
+                    lexemes.add(new Lexeme(LexemeType.OP_PLUS, c));
+                    pos++;
+                    continue;
+                case '-':
+                    lexemes.add(new Lexeme(LexemeType.OP_MINUS, c));
+                    pos++;
+                    continue;
+                case '*':
+                    lexemes.add(new Lexeme(LexemeType.OP_MUL, c));
+                    pos++;
+                    continue;
+                case '/':
+                    lexemes.add(new Lexeme(LexemeType.OP_DIV, c));
+                    pos++;
+                    continue;
+                default:
+                    if (c <= '9' && c >= '0') {
+                        StringBuilder sb = new StringBuilder();
+                        do {
+                            sb.append(c);
+                            pos++;
+                            if (pos >= expText.length()) {
+                                break;
+                            }
+                            c = expText.charAt(pos);
+                        } while (c <= '9' && c >= '0');
+                        lexemes.add(new Lexeme(LexemeType.NUMBER, sb.toString()));
+                    } else {
+                        if (c != ' ') {
+                            throw new RuntimeException("Unexpected character: " + c);
+                        }
+                        pos++;
+                    }
+            }
+        }
+        lexemes.add(new Lexeme(LexemeType.EOF, ""));
+        return lexemes;
+    }
+
+    public static int expr(LexemeBuffer lexemes) {
+        Lexeme lexeme = lexemes.next();
+        if (lexeme.type == LexemeType.EOF) {
+            return 0;
+        } else {
+            lexemes.back();
+            return plusminus(lexemes);
+        }
+    }
+
+    public static int plusminus(LexemeBuffer lexemes) {
+        Boolean bool = new Boolean();
+        int value = multdiv(lexemes);
+        while (true) {
+            Lexeme lexeme = lexemes.next();
+            switch (lexeme.type) {
+                case OP_PLUS:
+                    value += multdiv(lexemes);
+                    break;
+                case OP_MINUS:
+                    value -= multdiv(lexemes);
+                    break;
+                case EOF:
+                case RIGHT_BRACKET:
+                    lexemes.back();
+                    return value;
+                default:
+                    System.out.println("Error");
+                    bool.bool = false;
+                    System.out.println(bool.bool);
+            }
+        }
+    }
+
+    public static int multdiv(LexemeBuffer lexemes) {
+        Boolean bool = new Boolean();
+        int value = factor(lexemes);
+        while (true) {
+            Lexeme lexeme = lexemes.next();
+            switch (lexeme.type) {
+                case OP_MUL:
+                    value *= factor(lexemes);
+                    break;
+                case OP_DIV:
+                    value /= factor(lexemes);
+                    break;
+                case EOF:
+                case RIGHT_BRACKET:
+                case OP_PLUS:
+                case OP_MINUS:
+                    lexemes.back();
+                    return value;
+                default:
+                    System.out.println("Error");
+                    bool.bool = false;
+                    System.out.println(bool.bool);
+            }
+        }
+    }
+
+    public static int factor(LexemeBuffer lexemes) {
+        Boolean bool = new Boolean();
+        Lexeme lexeme = lexemes.next();
+        switch (lexeme.type) {
+            case NUMBER:
+                return Integer.parseInt(lexeme.value);
+            case LEFT_BRACKET:
+                int value = plusminus(lexemes);
+                lexeme = lexemes.next();
+                if (lexeme.type != LexemeType.RIGHT_BRACKET) {
+                    System.out.println("Error");
+                    bool.bool = false;
+                    System.out.println(bool.bool);
+                }else {
+                    bool.bool=true;
+                    System.out.println(bool.bool);
+                    return value;
+                }
+            default:
+                System.out.println("Error");
+                bool.bool = false;
+                System.out.println(bool.bool);
+
+        }
+        return 0;
+    }
+
+
+}
